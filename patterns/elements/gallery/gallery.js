b633b77426b5d3427715b1360a75e4a451f45b7c
danielwirz <d.wirz@iqual.ch>
2020-03-27 19:59:39 +0100

    fixed goruping issue

--- patterns/elements/gallery/gallery.js
+++ patterns/elements/gallery/gallery.js
@@ -1,6 +1,9 @@
 (function ($, Drupal) {
-  $(document).on('click', '.iq-gallery [data-toggle="lightbox-gallery"]', function (e) {
-    e.preventDefault();
-    $(this).ekkoLightbox();
-  });
+  $('.iq-gallery').each(function(){
+    $(this).find('a').attr('data-gallery', 'gallery-' + $(this).attr('id') );
+    $(document).on('click', '.iq-gallery [data-gallery="gallery-' + $(this).attr('id') + '"]', function (e) {
+      e.preventDefault();
+      $(this).ekkoLightbox();
+    });
+  })
 })(jQuery, Drupal);
