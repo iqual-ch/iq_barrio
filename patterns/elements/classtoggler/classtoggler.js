ebeea0c817fc8cfee7023f4cc57598bdf29fe336
Daniel Wirz <d.wirz@iqual.ch>
2021-03-09 10:42:23 +0100

    reworked toggler patterns

--- patterns/elements/classtoggler/classtoggler.js
+++ patterns/elements/classtoggler/classtoggler.js
@@ -1,11 +1,11 @@
 (function ($) {
-    $(document).ready(function() {
-        $('[data-toggle]').on('click', function () {
-            if( $(this).data('scroll-lock') == 'yes' ){
-                $('body').toggleClass('scroll-lock');
-            }
-            $( $(this).data('toggle') ).toggleClass($(this).data('class'));
-            $('[data-toggle="' + $(this).data('toggle') + '"]').toggleClass('active');
-        });
+  $(document).ready(function() {
+    $('[data-toggle]').on('click', function () {
+      if( $(this).data('scroll-lock') == 'yes' ){
+        $('body').toggleClass('scroll-lock');
+      }
+      $( $(this).data('toggle') ).toggleClass($(this).data('class'));
+      $('[data-toggle="' + $(this).data('toggle') + '"]').parent().toggleClass('active');
     });
+  });
 })(jQuery);
