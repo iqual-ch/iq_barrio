1c38838bd37e361e5617d8e139d15286528d9b36
Daniel Wirz <d.wirz@iqual.ch>
2021-06-21 09:07:14 +0200

    coding standards

--- patterns/composite/collapsible/collapsible.js
+++ patterns/composite/collapsible/collapsible.js
@@ -28,16 +28,10 @@
         }
         stack.remove();
       }
-
-
-
     });
     if (first != null) {
       first.accordion(definition);
     }
-
-
-
   }
 
 
@@ -47,56 +41,45 @@
     var first = null;
     var newGroup = true;
     $stacks.each(function (i, tab) {
-        tab = $(tab);
-        tab.removeClass('row').removeClass('iq-row');
-        if (tab.hasClass('iq-processed-tab')) {
-            return;
+      tab = $(tab);
+      tab.removeClass('row').removeClass('iq-row');
+      if (tab.hasClass('iq-processed-tab')) {
+        return;
+      }
+      tab.addClass('iq-processed-tab');
+      var id = tab.children('.iq-column').first().attr('id');
+
+      tab.find('li.iq-tab-title a').attr('href', '#' + id);
+      tab.find('li.iq-tab-title a').html(tab.find('li.iq-tab-title a').text());
+      tab.find('.iq-tab-text').attr('id', id);
+
+      if (newGroup) {
+        if (first != null) {
+          first.tabs(definition);
         }
-        tab.addClass('iq-processed-tab');
-        var id = tab.children('.iq-column').first().attr('id');
-
-        tab.find('li.iq-tab-title a').attr('href', '#' + id);
-        tab.find('li.iq-tab-title a').html( tab.find('li.iq-tab-title a').text() );
-        tab.find('.iq-tab-text').attr('id', id);
-
-        if (newGroup) {
-            if (first != null) {
-                first.tabs(definition);
-            }
-            first = tab;
-            if (!tab.next().hasClass('iq-tab-element')) {
-                newGroup = true;
-            } else {
-                newGroup = false;
-            }
+        first = tab;
+        if (!tab.next().hasClass('iq-tab-element')) {
+          newGroup = true;
         } else {
-          first.find('.iq-tab-list').append(tab.find('.iq-tab-title')).after(tab.children('.iq-column'));
-            if (!tab.next().hasClass('iq-tab-element')) {
-                newGroup = true;
-            }
-            tab.remove();
+          newGroup = false;
         }
+      } else {
+        first.find('.iq-tab-list').append(tab.find('.iq-tab-title')).after(tab.children('.iq-column'));
+        if (!tab.next().hasClass('iq-tab-element')) {
+          newGroup = true;
+        }
+        tab.remove();
+      }
 
     });
     if (first != null) {
-        first.tabs(definition);
+      first.tabs(definition);
     }
 
-    $('.iq-processed-collapsible').each(function(){
+    $('.iq-processed-collapsible').each(function () {
       $(this).children().wrapAll('<div class="col iq-column"></div>')
     });
-
-
-
   }
-
-
-
-
-
-
-
-
 
   Drupal.behaviors.iqual_pattern_collapsible = {
     attach: function (context, settings) {
@@ -108,7 +91,5 @@
       }
     }
   };
-
-
 
 })(jQuery, Drupal);
