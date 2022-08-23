883073fc5a5529e2430bbe059e4eb80399fd018d
Daniel Wirz <d.wirz@iqual.ch>
2022-08-23 14:55:02 +0200

    anchor rework

--- patterns/elements/anchor/anchor.js
+++ patterns/elements/anchor/anchor.js
@@ -1,82 +1,48 @@
+
+
 (function ($) {
-  function anchor($anchors) {
 
-    // Select all links with hashes
-    $anchors.click(function (event) {
-      // On-page links
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        // Figure out element to scroll to
-        let $target = $(this.hash);
-        $target = $target.length ? $target : $('[name=' + this.hash.slice(1) + ']');
+  window.scollOffset = 0
 
-        let hash = $(this).attr('href');
-        // Does a scroll target exist?
-        if ($target.length) {
-
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-
-          if (!$target.is(':visible')) {
-            // Check if target element is inside accordion.
-            if ($target.closest('.ui-accordion-content').length) {
-              $target.closest('.ui-accordion-content').prev('.ui-accordion-header').click();
-            }
-
-            // Check if target element is inside tab.
-            if ($target.closest('.iq-tab-content').length) {
-              let tabId = $target.closest('.iq-tab-content').attr('aria-labelledby');
-              $target.closest('.iq-tab-element').children('.iq-tab-list').children('.iq-tab-title[aria-labelledby="' + tabId + '"]').find('a').click();
-
-              setTimeout(function () {
-                history.pushState(null, null, document.location.pathname + hash);
-              }, 500);
-
-              // Do not scroll - jQuery UI already does that.
-              return;
-            }
-          }
-
-          let scrollPosition = $target.offset().top;
-          if ($('[data-anchornavigation]').length) {
-            scrollPosition -= $('[data-anchornavigation]').height();
-          }
-          $('html, body').animate({
-            scrollTop: scrollPosition
-          }, 500, function () {
-            history.pushState(null, null, document.location.pathname + hash);
-            // Callback after animation
-            // Must change focus!
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            }
-            else {
-              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            };
-          });
-        }
-      }
-    });
-  }
-  Drupal.behaviors.iqual_pattern_anchor = {
-    attach: function (context, settings) {
-      anchor($(context).find('.pd-live a[href *= "#"]').not('[href="#"]').not('[href="#0"]'));
-      $(document).ready(function () {
-        setTimeout(function(){
-          let $activeAnchor = $(location.hash);
-          let scrollPosition = $activeAnchor.offset().top;
-          if ($('[data-anchornavigation]').length) {
-            scrollPosition -= $('[data-anchornavigation]').height();
-          }
-
-          if ($activeAnchor.length && $activeAnchor.is(':visible')) {
-            $('html, body').animate({
-              scrollTop: scrollPosition
-            }, 500);
-          }
-        },500);
-      });
+  // Set scroll offset according to highest fixed element.
+  $('*').filter(function () {
+    if (!$(this).is(':visible')) {
+      return false;
     }
-  };
+    if ($(this).css('position') == 'fixed') {
+      return true;
+    }
+    if ($(this).hasClass('iq-anchornavigation') && $(this).data('position') == 'sticky' && $(this).data('align') == 'top') {
+      return true;
+    }
+  }).each(function(){
+    window.scollOffset = Math.max(window.scollOffset, $(this).height() + parseInt($(this).css('top')));
+  });
+
+
+  $(document).ready(function () {
+    setTimeout(function(){
+      anchorScroll();
+    }, 200);
+
+    window.onhashchange = function(e) {
+      anchorScroll();
+    };
+  });
+
+  window.anchorScroll = function() {
+    let hash = window.location.hash;
+    let $target = $(hash);
+
+    if (!$target.is(':visible')) {
+      let collapsibleId = $target.closest('[aria-labelledby]').attr('aria-labelledby');
+      if (collapsibleId) {
+        $('#' + collapsibleId).click();
+      }
+    }
+
+    let scrollPosition = $target.offset().top - window.scollOffset;
+    window.scrollTo(0, scrollPosition);
+  }
+
 })(jQuery);
--- patterns/elements/anchornavigation/anchornavigation.js
+++ patterns/elements/anchornavigation/anchornavigation.js
@@ -1,79 +1,48 @@
 (function ($) {
+
   $(document).ready(function () {
 
     $('[data-anchornavigation]').each(function(){
-      var $navigation_bar = $(this);
-      var html = '';
+      let $navbar = $(this);
+      $navbar.html('');
       $('[data-anchor-label]').each(function () {
-        html += '<a href="#' + $(this).attr('id') + '">';
-        html += '<span>' + $(this).data('anchor-label') + '</span>';
+        let $a = $('<a href="#' + $(this).attr('id') + '">');
+        $a.append('<span>' + $(this).data('anchor-label') + '</span>');
         if ($(this).data('anchor-icon') ) {
-          html += '<i class="' + $(this).data('anchor-icon') + '"></i>';
-        }
-        html += '</a>';
-      });
-      $navigation_bar.html( html );
-    });
-
-    $('[data-anchornavigation]').find('a').click(function (e) {
-      e.preventDefault();
-      let $target = $($(this).attr('href'));
-      let hash = $(this).attr('href');
-      if (!$target.is(':visible')) {
-
-        // Check if target element is inside accordion.
-        if ($target.closest('.ui-accordion-content').length) {
-          $target.closest('.ui-accordion-content').prev('.ui-accordion-header').click();
+          $a.append('<i class="' + $(this).data('anchor-icon') + '"></i>');
         }
 
-        // Check if target element is inside tab.
-        if ($target.closest('.iq-tab-content').length) {
-          let tabId = $target.closest('.iq-tab-content').attr('aria-labelledby');
-          $target.closest('.iq-tab-element').children('.iq-tab-list').children('.iq-tab-title[aria-labelledby="' + tabId + '"]').find('a').click();
-
-          setTimeout(function(){
-            history.pushState(null, null, document.location.pathname + hash);
-          }, 500);
-
-          // Do not scroll - jQuery UI already does that.
-          return;
-        }
-      }
-
-      if ($target.is(':visible')) {
-        $([document.documentElement, document.body]).animate({
-          scrollTop: $target.offset().top - $('[data-anchornavigation]').height()
-        }, 500, function() {
-          history.pushState(null, null, document.location.pathname + hash);
+        $a.click(function(){
+          if ($(this).attr('href') == window.location.hash) {
+            anchorScroll();
+          }
         });
-      }
-
-    });
-
-    var menuItems = $('[data-anchornavigation]').find('a');
-
-    var scrollItems = $('[data-anchornavigation]').first().find('a').map(function () {
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
-
-    // Bind to scroll
-    $(window).scroll(function () {
-      // Get container scroll position
-      var fromTop = $(this).scrollTop() + $('[data-anchornavigation]').height() + 20;
-
-      // Get id of current scroll item
-      var cur = scrollItems.map(function () {
-        if ($(this).is(':visible') && $(this).offset().top < fromTop)
-          return this;
+        $navbar.append($a);
       });
-      // Get the id of the current element
-      cur = cur[cur.length - 1];
-      var id = cur && cur.length ? cur[0].id : "";
-      // Set/remove active class
 
-      menuItems.removeClass('active').filter("[href='#" + id + "']").addClass("active");
+      let $menuItems = $navbar.find('a');
+      let scrollItems = $menuItems.map(function () {
+        let item = $($(this).attr("href"));
+        if (item.length) { return item; }
+      });
+
+      // Bind to scroll
+      $(window).scroll(function () {
+        // Get container scroll position
+        let fromTop = $(this).scrollTop();
+
+        // Get id of current scroll item
+        let cur = scrollItems.map(function () {
+          if ($(this).is(':visible') && $(this).offset().top - window.scollOffset <= fromTop + 1)
+            return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        let id = cur && cur.length ? cur[0].id : "";
+        // Set/remove active class
+
+        $menuItems.removeClass('active').filter("[href='#" + id + "']").addClass("active");
+      });
     });
-
   });
 })(jQuery);
