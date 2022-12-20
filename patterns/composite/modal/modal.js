bb4e65cb7077bbc3bd5c38cd920ef91ec85f5296
Daniel Wirz <d.wirz@iqual.ch>
2022-12-20 13:53:49 +0100

    working version

---
+++ patterns/composite/modal/modal.js
@@ -0,0 +1,55 @@
+/**
+ * @file
+ *
+ */
+
+ (function ($, Drupal) {
+  Drupal.behaviors.iqual_pattern_modal = {
+    attach: function (context, settings) {
+      $('[data-modal-open-action]').each(function(){
+
+        if ($(this).data('gjs-type') == 'modal') {
+          return;
+        }
+
+        $(this).find('[data-pd-editmode-toggle-modal]').remove();
+        let $self = $(this);
+        let action = $(this).data('modal-open-action');
+        let removeButton = true;
+
+        let $btnModalClose = $('[data-modal-close]');
+        let $modalContent = $('[data-modal-content]');
+        let $modalOverlay = $('[data-modal-overlay]');
+
+        $modalContent.append($btnModalClose);
+        $modalOverlay.append($modalContent);
+
+        $btnModalClose.click(function(){
+          $modalOverlay.removeClass('active');
+        })
+
+        switch (action) {
+          case 'button':
+            removeButton = false;
+            $self.find('[data-open-modal]').click(function(){
+              $modalOverlay.addClass('active');
+            });
+            break;
+          case 'load':
+            $modalOverlay.addClass('active');
+            break;
+        }
+
+        if (removeButton) {
+          $('[data-open-modal]').remove();
+        }
+
+        $(this).removeAttr('data-initialized')
+
+      });
+    }
+  };
+
+
+})(jQuery, Drupal);
+
